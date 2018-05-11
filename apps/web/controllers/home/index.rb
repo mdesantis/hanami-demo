54063544800e42ef0e9246967341846f07d6c0f1
Maurizio De Santis <desantis.maurizio@gmail.com>
2018-05-11 16:05:37 +0200

    Custom home page step

---
+++ apps/web/controllers/home/index.rb
@@ -0,0 +1,9 @@
+# apps/web/controllers/home/index.rb
+module Web::Controllers::Home
+  class Index
+    include Web::Action
+
+    def call(params)
+    end
+  end
+end
---
+++ spec/web/features/visit_home_spec.rb
@@ -0,0 +1,10 @@
+# spec/web/features/visit_home_spec.rb
+require 'features_helper'
+
+describe 'Visit home' do
+  it 'is successful' do
+    visit '/'
+
+    page.body.must_include('Bookshelf')
+  end
+end
